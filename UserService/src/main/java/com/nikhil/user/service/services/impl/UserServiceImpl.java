9c65e6d9deb658db7848ee5faaecf00453f719d8
Nikhil Sharma <sharmanikhilm05@gmail.com>
2022-12-24 16:29:59 +0530

    Fetching Rating for each user.

--- UserService/src/main/java/com/nikhil/user/service/services/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/nikhil/user/service/services/impl/UserServiceImpl.java
@@ -1,19 +1,28 @@
 package com.nikhil.user.service.services.impl;
 
+import com.nikhil.user.service.entities.Rating;
 import com.nikhil.user.service.entities.User;
 import com.nikhil.user.service.exceptions.ResourceNotFoundException;
 import com.nikhil.user.service.repository.UserRepository;
 import com.nikhil.user.service.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
-    UserRepository userRepository;
+    private UserRepository userRepository;
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private Logger logger= LoggerFactory.getLogger(UserServiceImpl.class);
     @Override
     public User saveUser(User user) {
         String randomUserId=UUID.randomUUID().toString();
@@ -23,11 +32,21 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> getAllUser() {
-        return userRepository.findAll();
+        List<User> users = userRepository.findAll();
+        for(var user:users)
+        {
+            ArrayList<Rating> ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/"+user.getUserId(), ArrayList.class);
+            user.setRatings(ratingsOfUser);
+        }
+        return users;
     }
 
     @Override
     public User getUser(String userId) {
-        return userRepository.findById(userId).orElseThrow(()->new ResourceNotFoundException("User with given id not found on tht server:"+userId));
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id not found on tht server:" + userId));
+        ArrayList<Rating> ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/"+user.getUserId(), ArrayList.class);
+        logger.info("{}",ratingsOfUser);
+        user.setRatings(ratingsOfUser);
+        return user;
     }
 }
